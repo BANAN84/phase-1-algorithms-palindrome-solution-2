7532706c8647d8882a2fc459db2beec6e0cb669c
banan <banan.faouri2001@gmail.com>
2023-10-11 14:39:02 +0300

    completed

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // creat a loop to go through each elemnt in the array until the middle
+  for ( let i=0; i< word.length/2; i++) {
+    
+    let j= word.length -1 -i;
+    if (word[i] == word[j]){
+       return true;
+    } 
+    else { return false  ;}
+  }
+   
 }
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here 
+  convert the word into an array 
+  make a loop to go through each elemnt in the array until the middle
+  compare first letter with the last one and the second one 
+  with the elemetn before the last one ...etc
+  return true if thy match and return false if they dont 
+
 */
 
 /*
@@ -13,6 +29,8 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+    /* diana => flase 
+     mom => true */
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
